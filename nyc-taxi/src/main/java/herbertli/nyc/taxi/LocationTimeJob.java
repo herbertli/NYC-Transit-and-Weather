6ebc1e37e6e9a8fec3477378dce8ba60a2f78839
Herbert Li <hl1785@nyu.edu>
2018-11-16 18:48:07 -0500

    additional code for joining

---
+++ nyc-taxi/src/main/java/herbertli/nyc/taxi/LocationTimeJob.java
@@ -0,0 +1,46 @@
+package herbertli.nyc.taxi;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class LocationTimeJob {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Usage LocationTimeJob <input_path> <output_path>");
+            System.exit(0);
+        }
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "analyzing nyc taxi data");
+        job.setJarByClass(LocationTimeJob.class);
+
+        if (args[0].contains("yellow") || args[0].contains("green")) {
+            job.setMapperClass(LocationTimeMapper.TaxiMapper.class);
+        } else {
+            job.setMapperClass(LocationTimeMapper.FHVMapper.class);
+        }
+
+        job.setCombinerClass(LocationTimeReducer.class);
+        job.setReducerClass(LocationTimeReducer.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+        FileInputFormat.addInputPath(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
---
+++ nyc-taxi/src/main/java/herbertli/nyc/taxi/LocationTimeMapper.java
@@ -0,0 +1,67 @@
+package herbertli.nyc.taxi;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class LocationTimeMapper {
+
+    private static final String TIMESTAMP_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIMESTAMP_PATTERN);
+
+    static class TaxiMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] split_line = value.toString().split(",");
+
+            LocalDateTime pickupTime = LocalDateTime.parse(split_line[0], formatter);
+            int dayOfWeek = pickupTime.getDayOfWeek().getValue();
+            int hourOfDay = pickupTime.getHour();
+            int minuteOfHour = pickupTime.getMinute();
+
+            int numPassenger;
+            if (StringUtils.isNumeric(split_line[2]))
+                numPassenger = Integer.parseInt(split_line[2]);
+            else
+                numPassenger = 1;
+
+            double tripDistance = Double.parseDouble(split_line[3]);
+            int pickupLoc = Integer.parseInt(split_line[4]);
+
+            String outKey = "";
+            outKey += dayOfWeek + " ";
+            outKey += String.format("%02d:%02d ", hourOfDay, minuteOfHour);
+            outKey += String.format("%d %f", pickupLoc, tripDistance);
+
+            context.write(new Text(outKey), new LongWritable(numPassenger));
+        }
+    }
+
+    static class FHVMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] split_line = value.toString().split(",");
+
+            LocalDateTime pickupTime = LocalDateTime.parse(split_line[1], formatter);
+            int dayOfWeek = pickupTime.getDayOfWeek().getValue();
+            int hourOfDay = pickupTime.getHour();
+            int minuteOfHour = pickupTime.getMinute();
+
+            int pickupLoc = Integer.parseInt(split_line[3]);
+
+            String outKey = "";
+            outKey += dayOfWeek + " ";
+            outKey += String.format("%02d:%02d ", hourOfDay, minuteOfHour);
+            outKey += pickupLoc;
+
+            context.write(new Text(outKey), new LongWritable(1));
+
+        }
+    }
+
+}
