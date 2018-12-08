a91673097f543a4400c2edb016d73ae250285ff3
Herbert Li <hl1785@nyu.edu>
2018-12-07 22:50:16 -0500

    fixes to fhv

--- source_code/nyc-taxi/src/main/java/LocationTimeMapper.java
+++ source_code/nyc-taxi/src/main/java/LocationTimeMapper.java
@@ -167,7 +167,7 @@ class LocationTimeMapper {
             if (pickupLoc.length() == 0 || !StringUtils.isNumeric(rowSplit[3]))
                 pickupLoc = "-1";
 
-            String dropoffLoc = rowSplit.get[4];
+            String dropoffLoc = rowSplit[4];
             if (dropoffLoc.length() == 0 || !StringUtils.isNumeric(rowSplit[4]))
                 dropoffLoc = "-1";
 
